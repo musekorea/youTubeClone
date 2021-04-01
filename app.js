d21c2915fed033c927792026cb6e42de23cc2bd5
musekorea <musekorea@hanmail.net>
2021-04-01 20:14:33 +0800

    mvc

--- app.js
+++ app.js
@@ -3,16 +3,11 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import { userRouter } from './router';
+import globalRouter from './routers/videoRouter';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
 
 const app = express();
-
-function handleHome(req, res) {
-  res.send(`🧛‍♂️  Hey!  You're in my Home Page`);
-}
-function handleLogin(req, res) {
-  res.send(`👮‍♂️ Hey!! Here is Login Page`);
-}
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -20,8 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan('dev'));
 
+app.use('/', globalRouter);
 app.use('/user', userRouter);
-app.get('/', handleHome);
-app.get('/login', handleLogin);
+app.use('/video', videoRouter);
 
 export default app;
