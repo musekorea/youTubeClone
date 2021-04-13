53630646984c105bd497f3981d74db5ccc22ac2a
musekorea <musekorea@hanmail.net>
2021-04-13 18:46:29 +0800

    mongodb-dotenv

--- init.js
+++ init.js
@@ -1,6 +1,9 @@
+import './db';
 import app from './app';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const handleListening = () => {
   console.log(`💚 Listening on: http://127.0.0.1:${PORT}`);
